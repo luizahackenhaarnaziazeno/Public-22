93b818d0b64bf2e798184b92a35c68c9996c0a36
Luiza Hackenhaar Naziazeno <142232479+luizahackenhaarnaziazeno@users.noreply.github.com>
2024-08-23 01:07:31 +0000

    pronto

---
+++ Nomes Jan-Mar.java
@@ -0,0 +1,9 @@
+public class Nomes Jan-Mar {
+    public static void main(String[] args) {
+        System.out.println("Luiza Hackenhaar Naziazeno ");
+        System.out.println("João Victor Farias de Souza");
+        System.out.println("Ianes Alencastro e Silva Filho");
+        System.out.println("Breno");
+        System.out.println("Pedro Augusto Soares");
+    }
+}
